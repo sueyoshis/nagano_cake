a57ebe703acc4681e581a03479fd5c5e70cc0e43
sueyoshis <99736235+sueyoshis@users.noreply.github.com>
2022-03-29 12:12:48 +0000

    [Add]配送先モデル・テーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_29_115554) do
+ActiveRecord::Schema.define(version: 2022_03_29_120726) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.integer "customer_id"
+    t.string "name"
+    t.string "postal_code"
+    t.string "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
