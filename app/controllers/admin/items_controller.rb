fc34c14202df74e32c8cacf87a7af82bb4b1254d
sueyoshis <99736235+sueyoshis@users.noreply.github.com>
2022-03-31 12:46:28 +0000

    [Update]商品一覧画面の作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,15 +2,16 @@ class Admin::ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-  
+
   def create
     @item = Item.new(item_params)
     @item.save
     redirect_to admin_item_path(@item.id)
-    
+
   end
 
   def index
+    @items = Item.all
   end
 
   def show
@@ -18,11 +19,11 @@ class Admin::ItemsController < ApplicationController
 
   def edit
   end
-  
+
   private
-  
+
   def item_params
     params.require(:item).permit(:image, :name, :intrduction, :genre_id, :price, :is_active)
   end
-  
+
 end
