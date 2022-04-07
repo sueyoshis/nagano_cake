3f41526161567864b183489440b4fd0e3904d229
sueyoshis <99736235+sueyoshis@users.noreply.github.com>
2022-04-07 13:31:29 +0000

    [Update]注文情報確認画面の作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,9 +1,25 @@
 class Public::OrdersController < ApplicationController
   def new
-    @customer = current_customer 
+    @customer = current_customer
+    @order = Order.new
   end
 
   def confirm
+    customer = current_customer
+    @cart_items = customer.cart_items
+    @sum = 0
+    @postage = 800
+    @order = Order.new(order_params)
+    if params[:order][:address_select] == "0"
+      @order.postal_code = customer.postal_code
+      @order.address = customer.address
+      @order.name = customer.full_name
+    elsif  params[:order][:address_select] == "1"
+      @address = Address.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+    end
   end
 
   def thanks
@@ -14,4 +30,11 @@ class Public::OrdersController < ApplicationController
 
   def show
   end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
+  end
+
 end
