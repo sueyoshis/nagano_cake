4b5166641a223d1a2f5320ffa1bb8500388bffd3
sueyoshis <99736235+sueyoshis@users.noreply.github.com>
2022-04-12 12:44:09 +0000

    [Update]顧客注文履歴一覧画面の作成

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,5 +4,6 @@ class Order < ApplicationRecord
   has_many :order_items, dependent: :destroy
 
   enum payment_method: { credit_card: 0, transfer: 1 }
+  enum status: { awaiting_paymant: 0, payment_confirmation: 1, production: 2, ready_to_ship: 3, sent: 4}
 
 end
