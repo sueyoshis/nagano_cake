174cc807ef819e8638f33f015dd6f086460fea53
sueyoshis <99736235+sueyoshis@users.noreply.github.com>
2022-04-15 10:59:16 +0000

    [Update]注文ステータス「入金確認」と製作ステータス「製作待ち」の連動

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -6,7 +6,10 @@ class Admin::OrdersController < ApplicationController
 
   def update
     order = Order.find(params[:id])
-    order.update(order_params)
+    if  order.update(order_params)
+      change_status_payment_confirmation = order.status_before_last_save == 'awaiting_paymant' and order.status == 'payment_confirmation'
+      order.order_items.update_all(production_status: 'waiting_for_production') if change_status_payment_confirmation
+    end
     redirect_to admin_order_path(order)
   end
 
