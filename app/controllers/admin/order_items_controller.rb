37edadf063ef8d33e64775161f351a63f2a0b7aa
sueyoshis <99736235+sueyoshis@users.noreply.github.com>
2022-04-15 11:32:36 +0000

    [Update]製作ステータス「製作中」と注文ステータス「製作中」の連動

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -2,7 +2,10 @@ class Admin::OrderItemsController < ApplicationController
 
   def update
     order_item = OrderItem.find(params[:order_id])
-    order_item.update(order_item_params)
+    if  order_item.update(order_item_params)
+      change_status_production = (order_item.production_status_before_last_save == 'waiting_for_production' and order_item.production_status == 'production')
+      order_item.order.update(status: 'production') if change_status_production
+    end
     redirect_to admin_order_path(order_item.order_id)
   end
 
