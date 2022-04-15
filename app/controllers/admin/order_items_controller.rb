3f0496e6be894f6f9c60ccbea8027bdf8eea36af
sueyoshis <99736235+sueyoshis@users.noreply.github.com>
2022-04-15 13:04:42 +0000

    [Update]製作ステータス「製作完了」と注文ステータス「発送準備中」の連動

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -5,6 +5,9 @@ class Admin::OrderItemsController < ApplicationController
     if  order_item.update(order_item_params)
       change_status_production = (order_item.production_status_before_last_save == 'waiting_for_production' and order_item.production_status == 'production')
       order_item.order.update(status: 'production') if change_status_production
+      change_status_production_completed = (order_item.production_status_before_last_save == 'production' and order_item.production_status == 'production_completed')
+      all_status_production_completed = order_item.order.order_items.all? {|order_item| order_item.production_status == 'production_completed'}
+      order_item.order.update(status: 'ready_to_ship') if change_status_production_completed and all_status_production_completed
     end
     redirect_to admin_order_path(order_item.order_id)
   end
