f05a147068a2998915a44ec01a960eb52569846b
sueyoshis <99736235+sueyoshis@users.noreply.github.com>
2022-04-17 11:02:59 +0000

    [Update]会員一覧画面のレイアウト調整

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -14,7 +14,7 @@ class Admin::CustomersController < ApplicationController
   def update
     @customer = Customer.find (params[:id])
     if @customer.update(customer_params)
-      redirect_to admin_customer_path(customer.id)
+      redirect_to admin_customer_path(@customer.id)
     else
       render :edit
     end
